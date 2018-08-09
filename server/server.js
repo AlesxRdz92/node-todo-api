61680120fa20e03c935f94f9b771117294ef0e43
Alejandro Gongora (Contractor) <x244911@LUV.AD.SWACORP.COM>
2018-08-09 14:15:33 -0500

    dd GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -20,10 +20,18 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then(todos => {
-        res.send({todos});
+        res.send({ todos });
     }, e => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    Todo.findById(req.params.id).then(todo => {
+        if (!todo)
+            return res.status(404).send('ID not found');
+        res.send(todo);
+    }, e => res.status(400).send(e.name));
 });
 
 app.listen(3000, () => {
