356d1cc754dfb3bd5f94b761fac9fe10635035b4
Alejandro Gongora (Contractor) <x244911@LUV.AD.SWACORP.COM>
2018-08-13 09:31:03 -0500

    Add PATCH /todos/id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { mongoose } = require('./db/mongoose');
@@ -40,6 +41,22 @@ app.delete('/todos/:id', (req, res) => {
             return res.status(404).send('ID not found');
         res.send({todo});
     }, e => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if(_.isBoolean(body.completed) && body.completed)
+        body.completedAt = new Date().getTime();
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+        if(!todo)
+            return res.status(404).send();
+        res.send({todo});
+    }).catch(e => res.status(400).send());
 });
 
 app.listen(port, () => {
