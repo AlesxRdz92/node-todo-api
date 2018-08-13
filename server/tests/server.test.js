5d73eafa73007b1212a34fb2bb73cfa636fbae94
Alejandro Gongora (Contractor) <x244911@LUV.AD.SWACORP.COM>
2018-08-13 10:56:52 -0500

    Add unit testing for PATCH /todos/id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 4
 }];
 
 beforeEach(done => {
@@ -94,32 +96,78 @@ describe('DELETE /todos/id', () => {
     it('should delete one element', done => {
         let id = todos[1]._id.toHexString();
         request(app).delete(`/todos/${id}`)
-        .expect(200)
-        .expect(res => {
-            expect(res.body.todo._id).toBe(id);
-        })
-        .end((err, res) => {
-            if(err)
-             return done(err);
-            Todo.findById(id).then(todo => {
-                expect(todo).toBeNull;
-                done();
-            }).catch(e => done(e));
-        });
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo._id).toBe(id);
+            })
+            .end((err, res) => {
+                if (err)
+                    return done(err);
+                Todo.findById(id).then(todo => {
+                    expect(todo).toBeNull();
+                    done();
+                }).catch(e => done(e));
+            });
     });
 
     it('should return a 400 code', done => {
         let id = '123abc';
         request(app).delete(`/todos/${id}`)
-        .expect(400)
-        .end(done);
+            .expect(400)
+            .end(done);
 
     });
 
     it('should return a 404 code', done => {
         let id = new ObjectID().toHexString();
         request(app).delete(`/todos/${id}`)
-        .expect(404)
-        .end(done);
+            .expect(404)
+            .end(done);
+    });
+});
+
+describe('PATCH /todos/id', () => {
+    it('should update the todo', done => {
+        let id = todos[0]._id.toHexString();
+        let text = 'Changed text';
+        request(app).patch(`/todos/${id}`)
+            .send({ text, completed: true, completedAt: 434253445 })
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.text).toEqual(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(typeof res.body.todo.completedAt).toBe('number');
+            })
+            .end(done);
+    });
+
+    it('should clear completedAt when todo is not completed', done => {
+        let id = todos[1]._id.toHexString();
+        let text = 'Changed text';
+        request(app).patch(`/todos/${id}`)
+            .send({ text, completed: false })
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.text).toEqual(text);
+                expect(res.body.todo.completed).toBeFalsy();
+                expect(res.body.todo.completedAt).toBeNull();
+            })
+            .end(done);
+    });
+
+    it('should return a 400 error code', done => {
+        let id = '123abc';
+        request(app).patch(`/todos/${id}`)
+            .send({ text: 'test' })
+            .expect(400)
+            .end(done);
+    });
+
+    it('should return a 400 error code', done => {
+        let id = new ObjectID().toHexString();
+        request(app).patch(`/todos/${id}`)
+            .send({ text: 'test' })
+            .expect(404)
+            .end(done);
     });
 });
